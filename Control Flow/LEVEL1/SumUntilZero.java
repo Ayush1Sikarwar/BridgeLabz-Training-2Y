4492e447673f9eb94a085dea590c6c328ce3c10f
Ayush1Sikarwar <ayushlegend1212@gmail.com>
2025-08-29 12:15:24 +0530

    Added LEVEL1 practice programs inside Control Flow

---
+++ Control Flow/LEVEL1/SumUntilZero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;
+        double num;
+
+        System.out.print("Enter numbers (0 to stop): ");
+        while ((num = sc.nextDouble()) != 0) {
+            total += num;
+        }
+
+        System.out.println("Total sum = " + total);
+        sc.close();
+    }
+}
